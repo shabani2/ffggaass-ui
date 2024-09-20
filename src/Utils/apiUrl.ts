6b6d687e2a1a759187881683a5cd1d950c4031bd
shabani2 <shabanibernard1@gmail.com>
2024-09-20 20:32:27 +0100

    setting up backend api dev url

--- src/Utils/apiUrl.ts
+++ src/Utils/apiUrl.ts
@@ -3,22 +3,27 @@ const VITE_API_DEV = import.meta.env.VITE_API_DEV;
 const VITE_API_PROD = import.meta.env.VITE_API_PROD;
 
 const getApiUrl = (): string => {
-    switch (window.location.hostname) {
-      case 'localhost':
-      case '127.0.0.1:9091/api/ffggaass/':
-        console.log('ffggaass is running on localhost !');
-        return VITE_API_LOCAL;
-      case 'https://inaf-backend-510563b7750d.herokuapp.com/api/ffggaass/':
-      case 'dev-ffggaass-api.web.app':
-        console.log('ffggaass is running on developement !');
-        return VITE_API_DEV;
-      case 'dgi.243technologies.com':
-        console.log('DGI is running on production !');
-        return VITE_API_PROD;
-      default:
-        console.log('DGI is runnign on : ', window.location.hostname);
-        return VITE_API_LOCAL;
-    }
-  };
-  
-  export default getApiUrl;
+  const hostname = window.location.hostname;
+
+  switch (hostname) {
+    case 'localhost':
+    case '127.0.0.1':
+      console.log('ffggaass is running on localhost!');
+      return VITE_API_LOCAL; // Utilise l'API locale pour le développement local
+
+    case 'inaf-backend-510563b7750d.herokuapp.com':
+    case 'dev-ffggaass-api.web.app':
+      console.log('ffggaass is running on development!');
+      return VITE_API_DEV; // Utilise l'API pour le développement
+
+    case 'dgi.243technologies.com':
+      console.log('DGI is running on production!');
+      return VITE_API_PROD; // Utilise l'API pour la production
+
+    default:
+      console.log('Unknown environment, defaulting to local API:', hostname);
+      return VITE_API_LOCAL; // Par défaut, retourne l'API locale
+  }
+};
+
+export default getApiUrl;
